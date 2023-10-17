71ae084372cb57b2e9b0f0db570d60f8d27e35d3
Lazar Pejovic <lazar.pejovic6@gmail.com>
2023-10-17 18:34:15 +0200

    Revert "Accede Project v 0.0.2"
    This reverts commit 7f45e3824d031a77887a5c528ec51d59072fac50.

--- src/main/java/project/accede/dto/role/RoleDTO.java
+++
@@ -1,9 +0,0 @@
-package project.accede.dto.role;
-
-import lombok.Data;
-
-@Data
-public class RoleDTO {
-
-    private int id;
-}
--- src/main/java/project/accede/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package project.accede.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import project.accede.entities.Role;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-}
